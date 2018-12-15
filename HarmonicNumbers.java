a9c4c1ebc03d7674ee3fac737c89ae5fa5db9287
parmeshwarraut1996 <45873296+parmeshwarraut1996@users.noreply.github.com>
2018-12-15 18:28:02 +0530

    Add files via upload

--- HarmonicNumbers.java
+++ HarmonicNumbers.java
@@ -1,8 +1,12 @@
 package com.bridgelabz.FunctionProg;
 
+import com.utility.Utility;
+
 public class HarmonicNumbers {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
+		Utility.getHarmonicNumberSeries();
 		// TODO Auto-generated method stub
 
 	}
